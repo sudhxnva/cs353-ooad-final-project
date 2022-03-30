ac9cc7355a1877bcfc193474904d373faf690c1a
Sudhanva M <sudhanva.m@pm.me>
2022-03-30 10:01:04 +0530

    Add start page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import StartPage from "./StartPage";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <StartPage />;
 }
---
+++ frontend/src/StartPage.js
@@ -0,0 +1,44 @@
+export default function StartPage() {
+  return (
+    <div className="py-12 bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="lg:text-center">
+          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+            CS352 Database Technologies
+          </h2>
+          <p className="mt-4 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            Spring MVC Project
+          </p>
+          <p className="mt-2 max-w-2xl text-m text-gray-500 lg:mx-auto">
+            Sudhanva M | PES2UG19CS410 | Section G
+          </p>
+        </div>
+      </div>
+      <div className="mt-20 flex items-center justify-center">
+        <div
+          className="inline-flex shadow-md hover:shadow-lg focus:shadow-lg"
+          role="group"
+        >
+          <button
+            type="button"
+            className="rounded-l inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out"
+          >
+            View Employees
+          </button>
+          {/* <button
+            type="button"
+            className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out"
+          >
+            Add Employee
+          </button> */}
+          <button
+            type="button"
+            className="rounded-r inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out"
+          >
+            Add Employee
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
