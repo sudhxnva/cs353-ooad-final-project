347995e4aa61d9cdf61842047a1cb34d883435bf
Sudhanva M <sudhanva.m@pm.me>
2022-03-30 08:46:32 +0530

    Add app test file

---
+++ backend/src/test/java/com/sudhanva/dbtproj/DbtprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudhanva.dbtproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbtprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
