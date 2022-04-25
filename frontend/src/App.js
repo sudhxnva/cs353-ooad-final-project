8987a79a56f3d15a58d9f401a2f84f507d635d94
Sudhanva M <sudhanva.m@pm.me>
2022-04-25 11:18:27 +0530

    Router refactor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,19 @@ import { ToastContainer } from "react-toastify";
 import Cart from "./Cart";
 import HomePage from "./HomePage";
 import ProductPage from "./Product";
+import NavBar from "./NavBar";
 
 export default function App() {
   return (
     <>
-      <ToastContainer />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/products/:id" element={<ProductPage />} />
-        <Route path="cart" element={<Cart />} />
-      </Routes>
+      <NavBar>
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products/:id" element={<ProductPage />} />
+          <Route path="cart" element={<Cart />} />
+        </Routes>
+      </NavBar>
     </>
   );
 }
--- frontend/src/HomePage.jsx
+++ frontend/src/HomePage.jsx
@@ -1,11 +1,6 @@
 import React from "react";
-import NavBar from "./NavBar";
 import Products from "./Products";
 
 export default function HomePage() {
-  return (
-    <NavBar>
-      <Products />
-    </NavBar>
-  );
+  return <Products />;
 }
