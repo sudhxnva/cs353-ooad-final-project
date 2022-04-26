4da3b8bf498f7f8203a06a535e4d39d87c7eb059
swayam-sa <swayam.blr@gmail.com>
2022-04-26 14:34:07 +0530

    product.json and sample images added

---
+++ backend/src/main/java/com/cs353/ooadproj/GetCredentials.java
@@ -0,0 +1,14 @@
+package com.cs353.ooadproj;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class GetCredentials extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        //TODO
+    }//end of doPost()
+}//end of class GetCredentials{}
